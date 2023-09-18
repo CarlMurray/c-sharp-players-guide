a0dda0e3114b1ceeaa0439e88b7406eae0ac9fc2
CarlMurray <carl.j.murray@gmail.com>
2023-09-18 09:48:24 +0100

    simulas test

---
+++ Simulas Test/Program.cs
@@ -0,0 +1,55 @@
+﻿PromptUser(State.Locked);
+
+void PromptUser(State state)
+{
+    string stateString = "";
+    while (stateString == "")
+    {
+        switch (state)
+        {
+            case State.Closed:
+                stateString = "closed";
+                break;
+            case State.Locked:
+                stateString = "locked";
+                break;
+            case State.Open:
+                stateString = "open";
+                break;
+            case State.Unlocked:
+                stateString = "unlocked";
+                break;
+        }
+    }
+
+    Console.WriteLine($"The chest is {stateString}. What do you want to do?");
+
+    string action = Console.ReadLine();
+
+    switch (action)
+    {
+        case "unlock":
+            state = State.Closed;
+            break;
+        case "open":
+            state = State.Open;
+            break;
+        case "close":
+            state = State.Closed;
+            break;
+        case "lock":
+            state = State.Locked;
+            break;
+    }
+
+    PromptUser(state);
+}
+
+
+enum State
+{
+    Locked,
+    Unlocked,
+    Closed,
+    Open
+};
