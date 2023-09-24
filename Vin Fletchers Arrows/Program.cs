1e36f53f411d45bf37a297721ffb849cfffaf62e
CarlMurray <carl.j.murray@gmail.com>
2023-09-24 12:29:59 +0100

    arrow factory

--- Vin Fletchers Arrows/Program.cs
+++ Vin Fletchers Arrows/Program.cs
@@ -2,41 +2,33 @@
 
 void CreateArrow()
 {
-    // GET ARROW HEAD
-    Console.WriteLine("Pick an arrow head type:");
-    Console.WriteLine("1: Steel");
-    Console.WriteLine("2: Wood");
-    Console.WriteLine("3: Obsidian");
+    Console.WriteLine("Which type of arrow do you want to make?");
+    Console.WriteLine("1: Elite Arrow");
+    Console.WriteLine("2: Beginner Arrow");
+    Console.WriteLine("3: Marksman Arrow");
+    Console.WriteLine("4: Custom Arrow");
+    int choice = Convert.ToInt32(Console.ReadLine());
 
-    ArrowHead arrowHead;
-    do
+    Arrow arrow;
+    switch (choice)
     {
-        arrowHead = (ArrowHead)(Convert.ToInt32(Console.ReadLine()) - 1);
-    } while ((int)arrowHead < 0 || (int)arrowHead > 2);
-
-    // GET FLETCHING TYPE
-    Console.WriteLine("Pick a fletching material:");
-    Console.WriteLine("1: Plastic");
-    Console.WriteLine("2: Turkey Feathers");
-    Console.WriteLine("3: Goose Feathers");
-    Fletching fletching;
-    do
-    {
-        fletching = (Fletching)(Convert.ToInt32(Console.ReadLine()) - 1);
-    } while ((int)fletching < 0 || (int)fletching > 2);
-
-    // GET LENGTH
-    Console.WriteLine("Enter a length between 60 - 100cm");
-    int length;
-    do
-    {
-        length = Convert.ToInt32(Console.ReadLine());
-    } while (length < 60 || length > 100);
-
-    Arrow arrow = new Arrow(arrowHead, fletching, length);
-
-    // GET COST
-    Console.WriteLine(arrow.GetCost());
+        case 1:
+            arrow = Arrow.CreateEliteArrow();
+            Console.WriteLine(arrow.GetCost());
+            break;
+        case 2:
+            arrow = Arrow.CreateBeginnerArrow();
+            Console.WriteLine(arrow.GetCost());
+            break;
+        case 3:
+            arrow = Arrow.CreateMarksmanArrow();
+            Console.WriteLine(arrow.GetCost());
+            break;
+        case 4:
+            arrow = Arrow.CreateCustomArrow();
+            Console.WriteLine(arrow.GetCost());
+            break;
+    }
 }
 
 class Arrow
@@ -45,6 +37,7 @@ class Arrow
     public Fletching Fletching { get; }
     public float Length { get; }
 
+    // CONCTRUCTOR
     public Arrow(ArrowHead arrowHead, Fletching fletching, float length)
     {
         ArrowHead = arrowHead;
@@ -94,6 +87,61 @@ class Arrow
 
         totalCost = arrowHeadCost + fletchingCost + lengthCost;
         return $"Total cost is {totalCost} gold.";
+    }
+
+    public static Arrow CreateEliteArrow()
+    {
+        return new Arrow(arrowHead: ArrowHead.Steel, fletching: Fletching.Plastic, length: 95);
+    }
+
+    public static Arrow CreateBeginnerArrow()
+    {
+        return new Arrow(arrowHead: ArrowHead.Wood, fletching: Fletching.GooseFeathers, length: 75);
+    }
+
+    public static Arrow CreateMarksmanArrow()
+    {
+        return new Arrow(arrowHead: ArrowHead.Steel, fletching: Fletching.GooseFeathers, length: 65);
+    }
+
+    public static Arrow CreateCustomArrow()
+    {
+        // GET ARROW HEAD
+        Console.WriteLine("Pick an arrow head type:");
+        Console.WriteLine("1: Steel");
+        Console.WriteLine("2: Wood");
+        Console.WriteLine("3: Obsidian");
+
+        ArrowHead arrowHead;
+        do
+        {
+            arrowHead = (ArrowHead)(Convert.ToInt32(Console.ReadLine()) - 1);
+        } while ((int)arrowHead < 0 || (int)arrowHead > 2);
+
+        // GET FLETCHING TYPE
+        Console.WriteLine("Pick a fletching material:");
+        Console.WriteLine("1: Plastic");
+        Console.WriteLine("2: Turkey Feathers");
+        Console.WriteLine("3: Goose Feathers");
+        Fletching fletching;
+        do
+        {
+            fletching = (Fletching)(Convert.ToInt32(Console.ReadLine()) - 1);
+        } while ((int)fletching < 0 || (int)fletching > 2);
+
+        // GET LENGTH
+        Console.WriteLine("Enter a length between 60 - 100cm");
+        int length;
+        do
+        {
+            length = Convert.ToInt32(Console.ReadLine());
+        } while (length < 60 || length > 100);
+
+        Arrow arrow = new Arrow(arrowHead, fletching, length);
+
+        // GET COST
+        Console.WriteLine(arrow.GetCost());
+        return arrow;
     }
 }
 
