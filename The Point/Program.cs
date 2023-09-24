29809ca5118f3df63114234b22e734c98e108818
CarlMurray <carl.j.murray@gmail.com>
2023-09-24 21:28:56 +0100

    the point boss

---
+++ The Point/Program.cs
@@ -0,0 +1,32 @@
+﻿Point point1 = new Point(2,3);
+Point point2 = new Point(-4,0);
+
+Console.WriteLine(point1.PrintPoint());
+Console.WriteLine(point2.PrintPoint());
+
+public class Point
+{
+    //Defines X, Y properties
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    //Constructor
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+    //Constructor with arguments
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    //Prints point formatted
+    public string PrintPoint()
+    {
+        return ($"({X}, {Y})");
+    }
+}
